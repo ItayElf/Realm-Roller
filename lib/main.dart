7fb4041297c5c2f383765152e8781b331d5af58c
ItayElf <thh123321@gmail.com>
2023-05-05 17:14:48 +0300

    Feat: prevented landscape mode

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:realm_roller/theme/theme_data.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]);
+
   runApp(const MyApp());
 }
 
