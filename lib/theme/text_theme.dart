452c05e021e78e1557b799e0cbb3044f164a39d5
ItayElf <thh123321@gmail.com>
2023-05-05 16:37:19 +0300

    Feat: added custom theme

---
+++ lib/theme/text_theme.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+final textTheme = TextTheme(
+  displayLarge: GoogleFonts.medievalSharp(
+    fontSize: 93,
+    fontWeight: FontWeight.w300,
+    letterSpacing: -1.5,
+  ),
+  displayMedium: GoogleFonts.medievalSharp(
+    fontSize: 58,
+    fontWeight: FontWeight.w300,
+    letterSpacing: -0.5,
+  ),
+  displaySmall: GoogleFonts.medievalSharp(
+    fontSize: 47,
+    fontWeight: FontWeight.w400,
+  ),
+  headlineMedium: GoogleFonts.medievalSharp(
+    fontSize: 33,
+    fontWeight: FontWeight.w400,
+    letterSpacing: 0.25,
+  ),
+  headlineSmall: GoogleFonts.medievalSharp(
+    fontSize: 23,
+    fontWeight: FontWeight.w400,
+  ),
+  titleLarge: GoogleFonts.medievalSharp(
+    fontSize: 19,
+    fontWeight: FontWeight.w500,
+    letterSpacing: 0.15,
+  ),
+  titleMedium: GoogleFonts.medievalSharp(
+    fontSize: 16,
+    fontWeight: FontWeight.w400,
+    letterSpacing: 0.15,
+  ),
+  titleSmall: GoogleFonts.medievalSharp(
+    fontSize: 14,
+    fontWeight: FontWeight.w500,
+    letterSpacing: 0.1,
+  ),
+  bodyLarge: GoogleFonts.roboto(
+    fontSize: 16,
+    fontWeight: FontWeight.w400,
+    letterSpacing: 0.5,
+  ),
+  bodyMedium: GoogleFonts.roboto(
+    fontSize: 14,
+    fontWeight: FontWeight.w400,
+    letterSpacing: 0.25,
+  ),
+  labelLarge: GoogleFonts.roboto(
+    fontSize: 14,
+    fontWeight: FontWeight.w500,
+    letterSpacing: 1.25,
+  ),
+  bodySmall: GoogleFonts.roboto(
+    fontSize: 12,
+    fontWeight: FontWeight.w400,
+    letterSpacing: 0.4,
+  ),
+  labelSmall: GoogleFonts.roboto(
+    fontSize: 10,
+    fontWeight: FontWeight.w400,
+    letterSpacing: 1.5,
+  ),
+);
---
+++ lib/theme/theme_data.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:realm_roller/theme/text_theme.dart';
+
+final themeData = ThemeData(
+  primarySwatch: const MaterialColor(0xFFFF2B0B, {
+    50: Color(0xFFFFEAEC),
+    100: Color(0xFFFFCACC),
+    200: Color(0xFFFA948E),
+    300: Color(0xFFF26861),
+    400: Color(0xFFFB4236),
+    500: Color(0xFFFF2B0B),
+    600: Color(0xFFF11A11),
+    700: Color(0xFFDF000B),
+    800: Color(0xFFD20001),
+    900: Color(0xFFC50000),
+  }),
+  textTheme: textTheme,
+  primaryTextTheme: textTheme,
+);
