0001e0012cf1fae3855e1b3c24850d3fb7df689c
ItayElf <thh123321@gmail.com>
2023-05-13 09:34:39 +0300

    Feat: added location generation page

--- lib/pages/general/generators.dart
+++ lib/pages/general/generators.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:realm_roller/custom_widgets/route_builder/route_builder.dart';
+import 'package:realm_roller/pages/locations/locations_generation/location_generation_page.dart';
 import 'package:realm_roller/pages/names/names_generation/names_generation_page.dart';
 import 'package:realm_roller/pages/npcs/npc_generation/npc_generation_page.dart';
 
@@ -12,6 +13,7 @@ class GeneratorsPage extends StatelessWidget {
     const generators = {
       "Npc": NpcGenerationPage(),
       "Names": NamesGenerationPage(),
+      "Locations": LocationGenerationPage()
     };
     final titles = generators.keys.toList();
 
