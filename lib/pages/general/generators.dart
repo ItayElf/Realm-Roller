c9e1137e38d62f95aea181d9b22a1f95d73a60fb
ItayElf <thh123321@gmail.com>
2023-06-02 19:34:18 +0300

    Feat: made generators page use generatorsData

--- lib/pages/general/generators.dart
+++ lib/pages/general/generators.dart
@@ -1,17 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:realm_roller/assets_handlers/generators_data.dart';
 import 'package:realm_roller/custom_widgets/route_builder/route_builder.dart';
-import 'package:realm_roller/pages/deities/deity_generation/deity_generation_page.dart';
-import 'package:realm_roller/pages/emblem/emblem_generation/emblem_generation_page.dart';
-import 'package:realm_roller/pages/guilds/guild_generation/guild_generation_page.dart';
-import 'package:realm_roller/pages/kingdoms/kingdom_generation/kingdom_generation_page.dart';
-import 'package:realm_roller/pages/landscapes/landscape_generation/landscape_generation_page.dart';
-import 'package:realm_roller/pages/locations/locations_generation/location_generation_page.dart';
-import 'package:realm_roller/pages/names/names_generation/npc_names_generation_page.dart';
-import 'package:realm_roller/pages/npcs/npc_generation/npc_generation_page.dart';
-import 'package:realm_roller/pages/settlements/settlements_generation/settlement_generation_page.dart';
-import 'package:realm_roller/pages/worlds/world_generation/world_generation_page.dart';
-
-import '../names/names_generation/settlement_name_generation.dart';
 
 /// A widget that lists all the available generators
 class GeneratorsPage extends StatelessWidget {
@@ -19,20 +8,7 @@ class GeneratorsPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    const generators = {
-      "Npc": NpcGenerationPage(),
-      "Names": NpcNamesGenerationPage(),
-      "Locations": LocationGenerationPage(),
-      "Settlements": SettlementGenerationPage(),
-      "Settlement Names": SettlementNamesGenerationPage(),
-      "Landscapes": LandscapeGenerationPage(),
-      "Deities": DeityGenerationPage(),
-      "Guilds": GuildGenerationPage(),
-      "Kingdoms": KingdomGenerationPage(),
-      "Emblems": EmblemGenerationPage(),
-      "World": WorldGenerationPage(),
-    };
-    final titles = generators.keys.toList();
+    final titles = generatorsData.keys.toList();
 
     return Scaffold(
       appBar: AppBar(
@@ -41,7 +17,7 @@ class GeneratorsPage extends StatelessWidget {
       ),
       body: ListView.separated(
         itemBuilder: (context, i) =>
-            _getGeneratorButton(context, titles[i], generators[titles[i]]!),
+            _getGeneratorButton(context, generatorsData[titles[i]]!),
         separatorBuilder: (context, i) => const SizedBox(
           height: 24,
         ),
@@ -51,12 +27,21 @@ class GeneratorsPage extends StatelessWidget {
   }
 
   Widget _getGeneratorButton(
-          BuildContext context, String title, Widget generatorPage) =>
+          BuildContext context, GeneratorData generatorData) =>
       ElevatedButton(
-          onPressed: () =>
-              Navigator.of(context).push(buildRoute(generatorPage)),
-          child: Text(
-            title,
-            style: Theme.of(context).textTheme.bodyLarge,
+          onPressed: () => Navigator.of(context)
+              .push(buildRoute(generatorData.generatorPage)),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Icon(generatorData.icon),
+              Text(
+                generatorData.title,
+                style: Theme.of(context)
+                    .textTheme
+                    .bodyLarge!
+                    .copyWith(color: Colors.white),
+              ),
+            ],
           ));
 }
