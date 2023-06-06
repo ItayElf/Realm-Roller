3477ebe25b04d2227be2c3b8ce3858f8e7ff65e1
ItayElf <thh123321@gmail.com>
2023-06-06 20:16:05 +0300

    Feat: added oracle generator

---
+++ lib/pages/general/menu_pages/oracle/oracle_generator.dart
@@ -0,0 +1,48 @@
+import 'package:randpg/generators.dart';
+
+enum OracleOdds {
+  almostCertain,
+  likely,
+  equallyLikely,
+  unlikely,
+  smallChance,
+}
+
+class OracleGenerator implements IGenerator<String> {
+  late int _seed;
+  final OracleOdds odds;
+
+  static const oddsToThresholds = {
+    OracleOdds.almostCertain: 10,
+    OracleOdds.likely: 25,
+    OracleOdds.equallyLikely: 50,
+    OracleOdds.unlikely: 75,
+    OracleOdds.smallChance: 90,
+  };
+
+  OracleGenerator(this.odds) {
+    _seed = SeedGenerator.generate();
+  }
+
+  @override
+  String generate() {
+    final threshold = oddsToThresholds[odds]!;
+
+    final diceResultGenerator = NumberGenerator(1, 101);
+    diceResultGenerator.seed(_seed);
+    final diceResult = diceResultGenerator.generate();
+
+    String result = diceResult > threshold ? "yes" : "no";
+
+    if (diceResult / 10 == diceResult % 10) {
+      result = "exceptional $result";
+    }
+
+    return result;
+  }
+
+  @override
+  void seed(int seed) {
+    _seed = seed;
+  }
+}
