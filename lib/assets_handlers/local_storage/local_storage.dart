a9e7e20c1c3ea3c05e98316e0cf799f3e908285a
ItayElf <thh123321@gmail.com>
2023-06-16 19:15:32 +0300

    Feat: added get all types for managers in local storage

--- lib/assets_handlers/local_storage/local_storage.dart
+++ lib/assets_handlers/local_storage/local_storage.dart
@@ -1,3 +1,13 @@
+import 'package:randpg/entities/deities.dart';
+import 'package:randpg/entities/emblems.dart';
+import 'package:randpg/entities/guilds.dart';
+import 'package:randpg/entities/kingdoms.dart';
+import 'package:randpg/entities/landscapes.dart';
+import 'package:randpg/entities/locations.dart';
+import 'package:randpg/entities/races.dart';
+import 'package:randpg/entities/settlements.dart';
+import 'package:randpg/entities/worlds.dart';
+import 'package:randpg/general.dart';
 import 'package:realm_roller/assets_handlers/local_storage/saver_data.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
@@ -53,5 +63,51 @@ abstract class LocalStorage {
     final type = entity.runtimeType;
     final entities = getEntities(type);
     return entities.contains(entity);
+  }
+
+  static getAvailableTypes(Manager manager) {
+    if (!managersToPaths.containsKey(manager)) {
+      throw Exception("No path to manager ${manager.runtimeType}");
+    }
+    final key = managersToPaths[manager]!;
+    final available = localStorage.getStringList(key);
+
+    if (available == null) {
+      final list = manager.allTypes.map(_getTypeKey).toList();
+      localStorage.setStringList(key, list);
+      return list;
+    }
+
+    return available;
+  }
+
+  static String _getTypeKey(dynamic type) {
+    switch (type.runtimeType) {
+      case DeityType:
+        return (type as DeityType).getDeityType();
+      case EmblemType:
+        return (type as EmblemType).getEmblemType();
+      case GuildType:
+        return (type as GuildType).getGuildType();
+      case KingdomType:
+        return (type as KingdomType).getKingdomType();
+      case GovernmentType:
+        return (type as GovernmentType).getGovernmentType();
+      case LandscapeType:
+        return (type as LandscapeType).getLandscapeType();
+      case LocationType:
+        return (type as LocationType).getLocationType();
+      case Race:
+        return (type as Race).getName();
+      case SettlementType:
+        return (type as SettlementType).getSettlementType();
+      case WorldSettings:
+        return (type as WorldSettings).getSettingName();
+      case WorldLoreType:
+        return (type as WorldLoreType).getLoreType();
+      case SvgWrapper:
+        return (type as SvgWrapper).name;
+    }
+    throw Exception("Cannot get key of type ${type.runtimeType}");
   }
 }
